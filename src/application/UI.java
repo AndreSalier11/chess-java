6fc78570c9324db2da8329c1056fb00c5459a1db
André Silva <andresalier11@gmail.com>
2023-05-10 15:42:56 +0100

    Added the possibility to move pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
@@ -25,6 +29,20 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+    public static ChessPosition readChessPosition(Scanner scan) {
+        try {
+            String s = scan.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+
+        } catch (RuntimeException e) {
+
+            throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
+        }
+    }
+
     
     public static void printBoard(ChessPiece[][] pieces) {
 
@@ -40,7 +58,7 @@ public class UI {
             System.out.println();
         }
 
-        System.out.print("  a b c d e f g h");
+        System.out.println("  a b c d e f g h");
     }
 
     private static void printPiece(ChessPiece piece) {
@@ -49,7 +67,7 @@ public class UI {
 
             System.out.print("-");
         } else {
-            
+
             if (piece.getColor() == Color.WHITE) {
 
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
